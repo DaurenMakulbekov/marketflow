ebcf257943b5be3c166a5ae3b6877285ea42de79
dmakulbe <dauren_d.m@mail.ru>
2025-08-21 11:31:17 +0500

    Add configuration

---
+++ internal/infrastructure/config/config.go
@@ -0,0 +1,83 @@
+package config
+
+import (
+	"os"
+)
+
+type DB struct {
+	Host     string
+	User     string
+	Password string
+	Name     string
+	Port     string
+}
+
+type Redis struct {
+	Addr     string
+	Password string
+}
+
+type Exchange struct {
+	Host string
+	Port string
+}
+
+type AppConfig struct {
+	DB        *DB
+	Redis     *Redis
+	Exchanges []*Exchange
+}
+
+func NewDB() *DB {
+	var db = &DB{
+		Host:     os.Getenv("DB_HOST"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Name:     os.Getenv("DB_NAME"),
+		Port:     os.Getenv("DB_PORT"),
+	}
+
+	return db
+}
+
+func NewRedis() *Redis {
+	var rdb = &Redis{
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+	}
+
+	return rdb
+}
+
+func NewExchanges() []*Exchange {
+	var exchanges []*Exchange
+
+	var exchange1 = &Exchange{
+		Host: os.Getenv("EXCHANGE1_HOST"),
+		Port: os.Getenv("EXCHANGE1_PORT"),
+	}
+
+	var exchange2 = &Exchange{
+		Host: os.Getenv("EXCHANGE2_HOST"),
+		Port: os.Getenv("EXCHANGE2_PORT"),
+	}
+
+	var exchange3 = &Exchange{
+		Host: os.Getenv("EXCHANGE3_HOST"),
+		Port: os.Getenv("EXCHANGE3_PORT"),
+	}
+
+	exchanges = append(exchanges, exchange1, exchange2, exchange3)
+
+	return exchanges
+}
+
+func NewAppConfig() *AppConfig {
+	var config = &AppConfig{
+		DB:        NewDB(),
+		Redis:     NewRedis(),
+		Exchanges: NewExchanges(),
+	}
+
+	return config
+}

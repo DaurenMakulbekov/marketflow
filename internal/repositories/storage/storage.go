9f4b697d6e26024bdea9b945d7c246d91f32d348
DaurenMakulbekov <dauren_d.m@mail.ru>
2025-08-26 17:18:46 +0500

    Update storage package

--- internal/repositories/storage/storage.go
+++ internal/repositories/storage/storage.go
@@ -5,15 +5,18 @@ import (
 )
 
 type storage struct {
-	table map[string]map[string][]domain.Exchange
+	table map[string]map[string]domain.Exchange
 }
 
 func NewStorage() *storage {
-	var table = make(map[string]map[string][]domain.Exchange)
+	var table = make(map[string]map[string]domain.Exchange)
 	var exchanges = []string{"exchange1", "exchange2", "exchange3"}
+	var symbols = []string{"BTCUSDT", "DOGEUSDT", "TONUSDT", "SOLUSDT", "ETHUSDT"}
 
 	for i := range exchanges {
-		table[exchanges[i]] = make(map[string][]domain.Exchange)
+		for j := range symbols {
+			table[exchanges[i]+":"+symbols[j]] = make(map[string]domain.Exchange)
+		}
 	}
 
 	return &storage{
@@ -22,17 +25,23 @@ func NewStorage() *storage {
 }
 
 func (st *storage) Write(exchange domain.Exchange) {
-	st.table[exchange.Exchange][exchange.Symbol] = append(st.table[exchange.Exchange][exchange.Symbol], exchange)
+	st.table[exchange.Exchange+":"+exchange.Symbol][exchange.ID] = exchange
 }
 
-func (st *storage) GetAll(exchanges []string) []domain.Exchange {
+func (st *storage) GetAll() []domain.Exchange {
 	var result []domain.Exchange
 
-	for i := range exchanges {
-		for k := range st.table[exchanges[i]] {
-			result = append(result, st.table[exchanges[i]][k]...)
+	for key := range st.table {
+		for _, v := range st.table[key] {
+			result = append(result, v)
 		}
 	}
 
 	return result
+}
+
+func (st *storage) DeleteAll(exchanges []domain.Exchange) {
+	for i := range exchanges {
+		delete(st.table[exchanges[i].Exchange+":"+exchanges[i].Symbol], exchanges[i].ID)
+	}
 }

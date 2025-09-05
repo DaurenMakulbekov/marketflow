c288657624976e14fcf24d717d66afcb5f4063f0
dmakulbe <dauren_d.m@mail.ru>
2025-09-05 13:19:14 +0500

    Update exchange handler

--- internal/handlers/exchangehandler/exchange_handler.go
+++ internal/handlers/exchangehandler/exchange_handler.go
@@ -21,10 +21,40 @@ func NewExchangeHandler(exchangeServ ports.ExchangeService) *exchangeHandler {
 
 func (exchangeHandl *exchangeHandler) LiveModeHandler(w http.ResponseWriter, req *http.Request) {
 	exchangeHandl.exchangeService.LiveMode()
+
+	var m = map[string]string{"Live mode": "active"}
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", " ")
+	err_ := encoder.Encode(m)
+	if err_ != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v", err_)
+		return
+	}
+
+	logger.Info("Live mode", "method", "POST", "status", 200)
 }
 
 func (exchangeHandl *exchangeHandler) TestModeHandler(w http.ResponseWriter, req *http.Request) {
 	exchangeHandl.exchangeService.TestMode()
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	var m = map[string]string{"Test mode": "active"}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", " ")
+	err_ := encoder.Encode(m)
+	if err_ != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v", err_)
+		return
+	}
+
+	logger.Info("Test mode", "method", "POST", "status", 200)
 }
 
 func (exchangeHandl *exchangeHandler) SystemStatusHandler(w http.ResponseWriter, req *http.Request) {
